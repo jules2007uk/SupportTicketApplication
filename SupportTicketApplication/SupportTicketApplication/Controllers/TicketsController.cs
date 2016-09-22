59d81358b2c2342263afac43c9669296e298e832
Julian Willing <Julian.Willing@popewoodhead.com>
2016-09-22 16:37:53 +0100

    Implemented Autofac to inject TicketRepository into Tickets controller

--- SupportTicketApplication/SupportTicketApplication/Controllers/TicketsController.cs
+++ SupportTicketApplication/SupportTicketApplication/Controllers/TicketsController.cs
@@ -9,7 +9,7 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace SupportTicketApplication.Controllers
-{
+{    
     public class TicketsController : Controller
     {
         #region Class level variables
@@ -36,7 +36,7 @@ namespace SupportTicketApplication.Controllers
             // call the Ticket repository to retrieve list of Ticket
             IList<Ticket> tickets = c_repository.RetrieveAllTickets();
             IList<TicketViewModel> ticketsAsViewModels = new List<TicketViewModel>();
-
+            
             // TODO: map the list of Ticket to the list of TicketViewModel
 
             // TODO: return the list of TicketViewModel
--- SupportTicketApplication/SupportTicketApplication/Global.asax.cs
+++ SupportTicketApplication/SupportTicketApplication/Global.asax.cs
@@ -1,7 +1,12 @@
-﻿using SupportTicketApplication.App_Start;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using DataAccess.Repositories;
+using SupportTicketApplication.App_Start;
+using SupportTicketApplication.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -15,7 +20,32 @@ namespace SupportTicketApplication
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);            
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var builder = new ContainerBuilder();            
+
+            // Register your MVC controllers.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // register the TicketRepository as the implementation of ITicketRepository
+            builder.RegisterType<TicketRepository>().As<ITicketRepository>();
+
+            //// OPTIONAL: Register model binders that require DI.
+            //builder.RegisterModelBinders(Assembly.GetExecutingAssembly());
+            //builder.RegisterModelBinderProvider();
+
+            //// OPTIONAL: Register web abstractions like HttpContextBase.
+            //builder.RegisterModule<AutofacWebTypesModule>();
+
+            //// OPTIONAL: Enable property injection in view pages.
+            //builder.RegisterSource(new ViewRegistrationSource());
+
+            //// OPTIONAL: Enable property injection into action filters.
+            //builder.RegisterFilterProvider();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
